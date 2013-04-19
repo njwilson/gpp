cd01d7336b726ad13e1bd6f95691b240a39c2ff1
Nick Wilson <nick@njwilson.net>
2013-04-19 15:14:25 -0500

    Use extended features for explore_params.sh

--- bin/explore_params.sh
+++ bin/explore_params.sh
@@ -8,7 +8,7 @@ seq_inc="$5"
 seq_last="$6"
 
 for cost in `seq $seq_first $seq_inc $seq_last`; do
-    output=`bin/gpp exp --train "$train_data" --eval "$eval_data" --method "$method" --cost $cost`
+    output=`bin/gpp exp --train "$train_data" --eval "$eval_data" --method "$method" --cost $cost --extended`
     accuracy=`echo "$output" |grep "Overall accuracy" | awk '{print $1}'`
     average_f1=`echo "$output" | grep "Average" | awk '{print $3}'`
     echo "${cost},${accuracy},${average_f1}"
